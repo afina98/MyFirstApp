570a061c7b6330719093dedb82e8d9dd06363237
afina98 <94692580+afina98@users.noreply.github.com>
2021-11-20 18:18:21 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,3 +8,11 @@ st.write('Disclamer. This web app is for learning purpose only')
 st.write ('Data is from [HERE](https://data.worldbank.org/indicator/SP.DYN.LE00.FE.IN?end=2019&locations=MY&most_recent_year_desc=false&start=1960&view=chart)')
 
 option = st.sidebar. selectbox('Select Country', ['Malaysia','Indonesia','Singapore'])
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
