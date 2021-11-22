0f5ed83f84c1d57aca5ac7965ce530e6aa3b4445
afina98 <94692580+afina98@users.noreply.github.com>
2021-11-22 12:55:57 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,8 @@ st.header("Life Expectancy at Birth, Female")
 st.write('Disclamer. This web app is for learning purpose only')
 st.write ('Data is from [HERE](https://data.worldbank.org/indicator/SP.DYN.LE00.FE.IN?end=2019&locations=MY&most_recent_year_desc=false&start=1960&view=chart)')
 
-df = pd.read_csv ('data\life_csv')
+df = pd.read_csv('lifedata_csv')
+
 option = st.sidebar. selectbox('Select Country', ['Malaysia','Indonesia','Singapore'])
 
 hide_streamlit_style = """
