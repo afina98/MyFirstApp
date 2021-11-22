26014330fd5c02be516f2416caa76bbd3b7b911c
afina98 <94692580+afina98@users.noreply.github.com>
2021-11-22 14:09:03 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,6 +6,17 @@ st.header("Life Expectancy at Birth, Female")
 st.write('Disclamer. This web app is for learning purpose only')
 st.write ('Data is from [HERE](https://data.worldbank.org/indicator/SP.DYN.LE00.FE.IN?end=2019&locations=MY&most_recent_year_desc=false&start=1960&view=chart)')
 
+st.write(pd.DataFrame({
+    'Country': ['Malaysia', 'Indonesia', 'Singapore'],
+    'Year 1960': [10,20,30],
+    'Year 1970': [10,60,30],
+    'Year 1980': [11,22,33],
+    'Year 1990': [33,44,55],
+    'Year 2000': [11,22,33],
+    'Year 2010': [33,44,55]
+
+}))
+            
 option = st.sidebar. selectbox('Select Country', ['Malaysia','Indonesia','Singapore'])
 
 hide_streamlit_style = """
