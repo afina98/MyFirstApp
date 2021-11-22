702cfd1d864629cd035f7b7a21008cf4993a236d
afina98 <94692580+afina98@users.noreply.github.com>
2021-11-22 12:57:57 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,6 +8,7 @@ st.write('Disclamer. This web app is for learning purpose only')
 st.write ('Data is from [HERE](https://data.worldbank.org/indicator/SP.DYN.LE00.FE.IN?end=2019&locations=MY&most_recent_year_desc=false&start=1960&view=chart)')
 
 df = pd.read_csv('lifedata_csv')
+st.write(df)
 
 option = st.sidebar. selectbox('Select Country', ['Malaysia','Indonesia','Singapore'])
 
